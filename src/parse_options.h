5b5e24397a960fd789c9bac9b8c6c0fe814b66f3
Camilo <ecamiloinacio@gmail.com>
2017-03-07 15:42:45 -0300

    Skeleton of the parse_options header

---
+++ src/parse_options.h
@@ -0,0 +1,19 @@
+/******************************************************************************\
+ * IORE - The IOR-Extended Benchmark
+ * 
+ * Author: Camilo <eduardo.camilo@posgrad.ufsc.br>
+ * Creation date: 2017-03-07
+ * License: GPLv3
+ * 
+ * Distributed Systems Research Laboratory (LAPESD)
+ * Federal University of Santa Catarina (UFSC)
+\******************************************************************************/
+
+#ifndef _PARSE_OPTIONS_H
+#define _PARSE_OPTIONS_H
+
+#include "iore.h"
+
+/* TODO: include declarations by demand */
+
+#endif /* _PARSE_OPTIONS_H */
